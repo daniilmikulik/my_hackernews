9159c28a6d645e1b132b0e639daa1775d10d3431
daniilmikulik <mikulik086420@gmail.com>
2022-01-19 19:55:46 +0300

    Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter,
-        Route,
-        Routes} from "react-router-dom";
-
 import './index.css';
-
-import MainPage from "./components/MainPage";
-import NewsPage from "./components/NewsPage";
-import NotFoundPage from "./components/NotFoundPage";
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<MainPage />} />
-              <Route path="/article/:articleId" element={<NewsPage/>} />
-              <Route path="/*" element={<NotFoundPage/>} />
-          </Routes>
-      </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
